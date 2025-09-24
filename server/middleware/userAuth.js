0519d350f7bd048872099afc64bf0b7cd91fb494
Gregory Kimani <gregkymgreg@gmail.com>
2025-09-24 11:26:06 +0300

    My MERN

---
+++ server/middleware/userAuth.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+
+const userAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.json({
+      status: false,
+      message: "User not Authorised please login again",
+    });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET_STR);
+
+    if (!decodedToken.id) {
+      return res.json({ status: false, message: "Not Authorised Login Again" });
+    }
+
+    if (!req.body) {
+      req.body = {};
+    }
+
+    req.body.userId = decodedToken.id;
+
+    next();
+  } catch (error) {
+    return res.json({ status: false, message: error.message });
+  }
+};
+
+export default userAuth;
